19f6fa9710d52fb1a2537c0909591e79d86e66df
fanyingdong <fanyd@ccccit.com.cn>
2024-01-25 14:54:47 +0800

    🐞 fix(background): 修改为仅在CQF学习页面生效

--- src/background.ts
+++ src/background.ts
@@ -1,6 +1,7 @@
 // background.ts
+const HOST = 'https://learn.cqf.com';
 (chrome as any).action.onClicked.addListener((tab: any) => {
-    if (tab.id) {
+    if (tab.url && tab.url.startsWith(HOST)) {
         (chrome as any).scripting.executeScript({
             target: { tabId: tab.id },
             files: ['inject.js']
